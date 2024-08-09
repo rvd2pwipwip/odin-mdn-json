243f3b3fb3a371514f60563534074ab8f2b59f47
Herve Desrosiers <herve_desrosiers@hotmail.com>
2024-08-09 10:28:42 -0400

    use JSON data in script.js and display it

--- script.js
+++ script.js
@@ -8,10 +8,63 @@ async function populate() {
 
   // make the network request using the fetch() function, and this returns a Response object
   const response = await fetch(request);
-  
+
+  // retrieve the response as a text string
+  const superHeroesText = await response.text();
+
   // retrieve the response as JSON using the json() function of the Response object
-  const superHeroes = await response.json();
+  // const superHeroes = await response.json();
+
+  // parse the text string response to a JS object
+  const superHeroes = JSON.parse(superHeroesText);
 
   populateHeader(superHeroes);
   populateHeroes(superHeroes);
 }
+
+function populateHeader(obj) {
+  const header = document.querySelector("header");
+  const myH1 = document.createElement("h1");
+  myH1.textContent = obj.squadName;
+  header.appendChild(myH1);
+
+  const myPara = document.createElement("p");
+  myPara.textContent = `Hometown: ${obj.homeTown} // Formed: ${obj.formed}`;
+  header.appendChild(myPara);
+}
+
+function populateHeroes(obj) {
+  const section = document.querySelector("section");
+  const heroes = obj.members;
+
+  for (const hero of heroes) {
+    const myArticle = document.createElement("article");
+    const myH2 = document.createElement("h2");
+    const myPara1 = document.createElement("p");
+    const myPara2 = document.createElement("p");
+    const myPara3 = document.createElement("p");
+    const myList = document.createElement("ul");
+
+    myH2.textContent = hero.name;
+    myPara1.textContent = `Secret identity: ${hero.secretIdentity}`;
+    myPara2.textContent = `Age: ${hero.age}`;
+    myPara3.textContent = "Superpowers:";
+
+    const superPowers = hero.powers;
+    for (const power of superPowers) {
+      const listItem = document.createElement("li");
+      listItem.textContent = power;
+      myList.appendChild(listItem);
+    }
+
+    myArticle.appendChild(myH2);
+    myArticle.appendChild(myPara1);
+    myArticle.appendChild(myPara2);
+    myArticle.appendChild(myPara3);
+    myArticle.appendChild(myList);
+
+    section.appendChild(myArticle);
+  }
+}
+
+populate();
